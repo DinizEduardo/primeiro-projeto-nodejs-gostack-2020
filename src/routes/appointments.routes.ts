e723edff061023356238526b5dc6897bf9d191c7
Eduardo Diniz <eduardo.diniz987@gmail.com>
2020-06-22 16:02:06 -0300

    Trabalhando com dados

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -28,7 +28,10 @@ appointmentRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked!' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
